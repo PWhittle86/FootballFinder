f933200084230d88f953acb48a4f98b6c3279458
Peter Whittle <peter.whittle@city-holdings.co.uk>
2020-09-13 23:44:25 +0100

    Attempting to add extension - XCode not liking it atm.

---
+++ HungrrrTechTest/Utilities/AlertUtility.swift
@@ -0,0 +1,20 @@
+//
+//  AlertUtility.swift
+//  HungrrrTechTest
+//
+//  Created by Peter Whittle on 13/09/2020.
+//  Copyright © 2020 Whittle Productions. All rights reserved.
+//
+
+import Foundation
+
+class AlertUtility {
+    
+    func presentAlert() {
+        
+        
+        
+        
+    }
+    
+}
